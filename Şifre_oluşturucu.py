31b81799fd4b7e3ec5c1cac68ff0d3aad84012ba
Tuana Koyun <meneksekoyun@gmail.com>
2024-10-30 20:19:07 +0300

    Create Şifre_oluşturucu.py

---
+++ Şifre_oluşturucu.py
@@ -0,0 +1,10 @@
+import random
+
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+sifre_uzunluğu = int(input("Sifre_uzunluğunu girin"))
+for i in range (sifre_uzunluğu):10
+    sifre +- random.choice(karakterler)
+
+
+print[sifre] 
+
